f38ef82f8b9800529b5683f949d5493aa6268ea9
Ozer <ozersubasi.dev@gmail.com>
2025-04-21 19:58:15 +0300

    created base

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import 'dotenv/config'
+import Fastify, {FastifyInstance, RouteShorthandOptions} from 'fastify'
+import cors from '@fastify/cors'
+
+const fastify: FastifyInstance = Fastify({
+    logger: true
+})
+
+fastify.register(cors, {
+    origin: '*',
+    methods: ['GET,PUT,POST,DELETE,OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 3600,
+})
+
+const opts: RouteShorthandOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    pong: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    }
+}
+
+fastify.get('/ping', opts, async () => {
+    return {pong: 'it worked!'}
+})
+
+const start = async () => {
+    try {
+        await fastify.ready()
+        await fastify.listen({port: Number(process.env.PORT)})
+
+        const address = fastify.server.address()
+        const port = typeof address === 'string' ? address : address?.port
+        console.log(`Server is running on http://${process.env.HOST}:${port}`);
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+(async () => {
+    try {
+        await start()
+    } catch (err) {
+        console.error('Error starting server:', err)
+        process.exit(1)
+    }
+})()
